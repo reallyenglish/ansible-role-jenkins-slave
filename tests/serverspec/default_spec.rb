7e6e8c05a5c69a24889912de4ef8f4a0393fb32b
Mitsuru Y <mitsuruy@reallyenglish.com>
2017-02-06 17:29:11 +0900

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,58 +1,57 @@
 require 'spec_helper'
 require 'serverspec'
 
-package = 'jenkins-slave'
-service = 'jenkins-slave'
-config  = '/etc/jenkins-slave/jenkins-slave.conf'
-user    = 'jenkins-slave'
-group   = 'jenkins-slave'
-ports   = [ PORTS ]
-log_dir = '/var/log/jenkins-slave'
-db_dir  = '/var/lib/jenkins-slave'
+package = 'jenkins'
+user    = 'jenkins'
+group   = 'jenkins'
+home    = '/var/lib/jenkins'
 
 case os[:family]
 when 'freebsd'
-  config = '/usr/local/etc/jenkins-slave.conf'
-  db_dir = '/var/db/jenkins-slave'
+  home= '/usr/local/jenkins'
 end
 
 describe package(package) do
   it { should be_installed }
 end 
 
-describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape('jenkins-slave') }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when 'freebsd'
-  describe file('/etc/rc.conf.d/jenkins-slave') do
-    it { should be_file }
-  end
-end
-
 describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
+  it { should_not be_running }
+  it { should_not be_enabled }
 end
 
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
-  end
+describe user(user) do
+  it { should exist }
+end
+
+describe group (group) do
+  it { should exist }
+end
+
+describe file(home) do
+  it { should exist }
+  it { should be_mode 755 }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+end
+
+describe file("#{home}/.ssh") do
+  it { should exist }
+  it { should be_mode 700 }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+end
+
+describe file("#{home}/.ssh/authorized_keys") do
+  it { should exist }
+  it { should be_mode 600 }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+end
+
+describe file("#{home}/.ansible_vault_key") do
+  it { should exist }
+  it { should be_mode 600 }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
 end
